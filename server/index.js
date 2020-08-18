22e136feb812e0b49cef3a7715b280204c67625a
BluePrince123 <turanskiserhii0228@gmail.com>
2020-08-18 14:51:47 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+// In order to use the MinIO JavaScript API to generate the pre-signed URL, begin by instantiating
+// a `Minio.Client` object and pass in the values for your server.
+// The example below uses values for play.min.io:9000
+
+const Minio = require('minio')
+
+var client = new Minio.Client({
+    endPoint: 'play.min.io',
+    port: 9000,
+    useSSL: true,
+    accessKey: 'Q3AM3UQ867SPQQA43P2F',
+    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+})
+
+// Instantiate an `express` server and expose an endpoint called `/presignedUrl` as a `GET` request that
+// accepts a filename through a query parameter called `name`. For the implementation of this endpoint,
+// invoke [`presignedPutObject`](https://docs.min.io/docs/javascript-client-api-reference#presignedPutObject) 
+// on the `Minio.Client` instance to generate a pre-signed URL, and return that URL in the response:
+
+// express is a small HTTP server wrapper, but this works with any HTTP server
+const server = require('express')()
+
+server.get('/presignedUrl', (req, res) => {
+    client.presignedPutObject('uploads', req.query.name, (err, url) => {
+        if (err) throw err
+        res.end(url)
+    })
+})
+
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+server.listen(8080)
